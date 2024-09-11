35facdc2243832afe2feb3dd52a67693b4816626
salih <salihasd99@gmail.com>
2024-09-11 17:16:38 +0300

    add firebase config

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,16 @@ import 'bluetooth_helper.dart';
 import 'connected_device_page.dart';
 import 'qr_scan_page.dart';
 
-void main() => runApp(const MyApp());
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
